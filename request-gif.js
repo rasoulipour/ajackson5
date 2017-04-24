d4e9fe676a2e2ecc2341e94c6e255f2eb79e5759
Rasoulipour <rasoulipourmohammad@gmail.com>
2017-04-23 21:43:28 -0500

    Ajaxon5

---
+++ request-gif.js
@@ -0,0 +1,88 @@
+
+
+$(document).ready(function() {
+    // register our function as the "callback" to be triggered by the form's submission event
+    $("#form-gif-request").submit(fetchAndDisplayGif); // in other words, when the form is submitted, fetchAndDisplayGif() will be executed
+});
+/**
+ * sends an asynchronous request to Giphy.com aksing for a random GIF using the
+ * user's search term (along with "jackson 5")
+ *
+ * upon receiving a response from Giphy, updates the DOM to display the new GIF
+ */
+function fetchAndDisplayGif(event) {
+
+    var test = document.getElementById("humantest").value;
+
+    if (test == 5){
+
+          //$("#testerror").attr("hidden", true);
+
+          // This prevents the form submission from doing what it normally does: send a request (which would cause our page to refresh).
+          // Because we will be making our own AJAX request, we dont need to send a normal request and we definitely don't want the page to refresh.
+          event.preventDefault();
+
+          // get the user's input text from the DOM
+          var x = document.getElementById("inputPhrase").value;
+          // TODO should be e.g. "dance"
+
+          // configure a few parameters to attach to our request
+          var params = {
+              api_key: "dc6zaTOxFJmzC",
+              tag : "jackson 5 " + x// TODO should be e.g. "jackson 5 dance"
+          };
+
+          // make an ajax request for a random GIF
+          $.ajax({
+              url: "https://api.giphy.com/v1/gifs/random", // TODO where should this request be sent?
+              data: params, // attach those extra parameters onto the request
+              success: function(response) {
+                  // if the response comes back successfully, the code in here will execute.
+
+                  // jQuery passes us the `response` variable, a regular javascript object created from the JSON the server gave us
+                  console.log("we received a response!");
+                  console.log(response);
+                  var myRandomUrl = response.data.image_url;
+                  document.getElementById("gif").setAttribute("src", myRandomUrl);
+                  console.log(params);
+                  $("#loadingmessage").attr("hidden", true);
+                  $("#gif").attr("hidden", false);
+
+
+
+
+              },
+              error: function() {
+                  // if something went wrong, the code in here will execute instead of the success function
+                  // give the user an error message
+                  $("#feedback").text("Sorry, could not load GIF. Try again!");
+                  setGifLoadedStatus(false);
+
+              }
+
+
+          });
+          document.getElementById('gif').hidden = true;
+          document.getElementById('loadingmessage').hidden = false;
+
+
+
+
+    } else {
+      document.getElementById('testerror').innerHTML = "No Gifs for you!";
+    }
+
+}
+
+
+
+/**
+ * toggles the visibility of UI elements based on whether a GIF is currently loaded.
+ * if the GIF is loaded: displays the image and hides the feedback label
+ * otherwise: hides the image and displays the feedback label
+ */
+ function setGifLoadedStatus(isCurrentlyLoaded) {
+     $("#gif").attr("hidden", !isCurrentlyLoaded);
+     $("#loadingmessage").attr("hidden", !isCurrentlyLoaded);
+     $("#feedback").attr("hidden", isCurrentlyLoaded);
+ }
